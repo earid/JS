1606d88f4d9bae784bcb85b59e0b8e669d0aa720
Earid A <earid777@gmail.com>
2023-04-27 03:06:45 +0600

    Create script.js

---
+++ DataTableDisplay/script.js
@@ -0,0 +1,15 @@
+fetch('https://fakestoreapi.com/products/').then((data) => {
+    return data.json();
+}).then((ObjectData) => {
+    let tableData = '';
+    ObjectData.map((value) => {
+        tableData += `<tr>
+        <td>${value.title}</td>
+        <td>${value.description}</td>
+        <td>${value.price}</td>
+        <td><img src='${value.image}'><img></td>
+      </tr>`
+    })
+    document.getElementById('tBody').innerHTML = tableData
+}
+)
